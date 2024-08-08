fed79bf5dc7113ef9cf07653874fe79c6225ec4c
Andrew Lipuma <andrewlipuma@gmail.com>
2024-08-07 17:01:31 -0700

    Add test for selecting supportive plan

--- test/specs/createAnOrder.e2e.js
+++ test/specs/createAnOrder.e2e.js
@@ -1,6 +1,19 @@
 const page = require('../../page');
 const helper = require('../../helper')
 
+/*
+Tests for setting the address and filling in the phone number appear to be done already
+
+I need to add tests for:
+- Selecting supportive plan (done)
+- Adding a credit card
+- Writing a message for the driver
+- Ordering a Blanket and handkerchiefs
+- Ordering 2 Ice creams
+- The car search modal appears
+- Waiting for the driver info to appear in the modal
+*/
+
 describe('Create an order', () => {
     it('should open phone number modal', async () => {
         await browser.url(`/`)
@@ -10,6 +23,15 @@ describe('Create an order', () => {
         await phoneNumberButton.click();
         const pnoneNumberModal = await $(page.phoneNumberModal);
         await expect(pnoneNumberModal).toBeExisting();
+    })
+
+    it('should select the supportive plan', async () => {
+        await browser.url('/')
+        await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
+        const supportiveButton = await $(".tariff-cards>:nth-child(5)") //Should select the supportive car option
+        await supportiveButton.waitForDisplayed();
+        await supportiveButton.click();
+        await expect(supportiveButton).toHaveElementClassContaining("tcard active"); //Checks to see if the supportive car option is active once clicked
     })
 
     it('should save the phone', async () => {
