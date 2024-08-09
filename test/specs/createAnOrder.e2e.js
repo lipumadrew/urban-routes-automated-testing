02914c17149516a932eee2f172b43605e7479e56
Andrew Lipuma <andrewlipuma@gmail.com>
2024-08-08 21:58:47 -0700

    Trying headless mode to see if that helps

--- test/specs/createAnOrder.e2e.js
+++ test/specs/createAnOrder.e2e.js
@@ -1,91 +1,83 @@
-const page = require('../../page');
-const helper = require('../../helper')
+const page = require("../../page");
+const helper = require("../../helper");
 
+describe("Create an order", () => {
+  it("should open phone number modal", async () => {
+    await browser.url(`/`);
+    await page.fillAddresses("East 2nd Street, 601", "1300 1st St");
+    const phoneNumberButton = await $(page.phoneNumberButton);
+    await phoneNumberButton.waitForDisplayed();
+    await phoneNumberButton.click();
+    const pnoneNumberModal = await $(page.phoneNumberModal);
+    await expect(pnoneNumberModal).toBeExisting();
+  });
 
+  it("should select the supportive plan", async () => {
+    await browser.url("/");
+    await page.fillAddresses("East 2nd Street, 601", "1300 1st St");
+    const supportiveButton = await $(".tariff-cards>:nth-child(5)"); //Should select the supportive car option
+    await supportiveButton.waitForDisplayed();
+    await supportiveButton.click();
+    await expect(supportiveButton).toHaveElementClassContaining("tcard active"); //Checks to see if the supportive car option is active once clicked
+  });
 
-describe('Create an order', () => {
-    it('should open phone number modal', async () => {
-        await browser.url(`/`)
-        await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
-        const phoneNumberButton = await $(page.phoneNumberButton);
-        await phoneNumberButton.waitForDisplayed();
-        await phoneNumberButton.click();
-        const pnoneNumberModal = await $(page.phoneNumberModal);
-        await expect(pnoneNumberModal).toBeExisting();
-    })
+  it("should add a card as a payment method", async () => {
+    await browser.url("/");
+    await page.fillAddresses("East 2nd Street, 601", "1300 1st St");
+    const paymentButton = await $(".pp-button");
+    await paymentButton.waitForDisplayed();
+    await paymentButton.click();
+    const addCardButton = await $(".pp-row.disabled");
+    await addCardButton.click();
+    const cardNumberField = await $("#number");
+    await cardNumberField.setValue("123412341234");
+    const cardCodeField = await $(".card-code-input #code");
+    await cardCodeField.setValue("321");
+    const linkButton = await $(".pp-buttons :nth-child(1)");
+    await linkButton.click();
+    const addedCard = await $("div.pp-selector > div:nth-child(3) > .pp-title");
+    await expect(addedCard).toHaveText("Card");
+  });
 
-    it('should select the supportive plan', async () => {
-        await browser.url('/')
-        await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
-        const supportiveButton = await $(".tariff-cards>:nth-child(5)") //Should select the supportive car option
-        await supportiveButton.waitForDisplayed();
-        await supportiveButton.click();
-        await expect(supportiveButton).toHaveElementClassContaining("tcard active"); //Checks to see if the supportive car option is active once clicked
-    })
+  it("should save the phone", async () => {
+    await browser.url(`/`);
+    await page.fillAddresses("East 2nd Street, 601", "1300 1st St");
+    const phoneNumber = helper.getPhoneNumber("+1");
+    await page.submitPhoneNumber(phoneNumber);
+    await expect(await helper.getElementByText(phoneNumber)).toBeExisting();
+  });
 
-    it('should add a card as a payment method', async () => {
-        await browser.url('/');
-        await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
-        const paymentButton = await $('.pp-button');
-        await paymentButton.waitForDisplayed();
-        await paymentButton.click();
-        const addCardButton = await $('.pp-row.disabled');
-        await addCardButton.click();
-        const cardNumberField = await $('#number');
-        await cardNumberField.setValue("123412341234");
-        const cardCodeField = await $(".card-code-input #code");
-        await cardCodeField.setValue("321");
-        const linkButton = await $(".pp-buttons :nth-child(1)");
-        await linkButton.click();
-        const addedCard = await $("div.pp-selector > div:nth-child(3) > .pp-title");
-        await expect(addedCard).toHaveText("Card");
+  it("should accept a message to the driver", async () => {
+    await browser.url(`/`);
+    await page.fillAddresses("East 2nd Street, 601", "1300 1st St");
+    const driverMessage = $("#comment");
+    await driverMessage.setValue("Please drive carefully");
+    await expect(driverMessage).toHaveValue("Please drive carefully");
+  });
 
-    })
+  it("should be able to order a blanket and hankerchiefs", async () => {
+    const firstSlider = $(".slider.round");
+    await firstSlider.click();
+  });
 
-    it('should save the phone', async () => {
-        await browser.url(`/`)
-        await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
-        const phoneNumber = helper.getPhoneNumber("+1");
-        await page.submitPhoneNumber(phoneNumber);
-        await expect(await helper.getElementByText(phoneNumber)).toBeExisting();
-    })
+  it("should be able to order 2 ice cream buckets", async () => {
+    const plusCounter = await $(".counter-plus");
+    await plusCounter.click();
+    await plusCounter.click();
+    await expect($(".counter-value")).toHaveText("2");
+  });
 
-    it('should accept a message to the driver', async () => {
-        await browser.url(`/`)
-        await page.fillAddresses('East 2nd Street, 601', '1300 1st St');
-        const driverMessage = $("#comment");
-        await driverMessage.setValue("Please drive carefully");
-        await expect(driverMessage).toHaveValue("Please drive carefully");
-    })
+  it("should open the car search modal", async () => {
+    const orderButton = await $(".smart-button");
+    await orderButton.click();
+    const orderModal = await $(".order-body");
+    await orderModal.waitForDisplayed();
+    await expect(orderModal).toBeDisplayed();
+  });
 
-    it('should be able to order a blanket and hankerchiefs', async () => {
-        const firstSlider = $(".slider.round");
-        await firstSlider.click();
-
-        
-    })
-
-    it('should be able to order 2 ice cream buckets', async () => {
-        const plusCounter = await $(".counter-plus");
-        await plusCounter.click();
-        await plusCounter.click();
-        await expect($(".counter-value")).toHaveText("2");
-
-    })
-
-    it('should open the car search modal', async () => {
-        const orderButton = await $(".smart-button");
-        await orderButton.click();
-        const orderModal = await $(".order-body");
-        await orderModal.waitForDisplayed();
-        await expect(orderModal).toBeDisplayed();
-    })
-
-    it('should display driver info in the modal', async () => {
-        const orderHeader = await $(".order-header-content");
-        await orderHeader.waitForDisplayed();
-        await expect(orderHeader).toBeDisplayed();
-        
-    })
-})
-
+  it("should display driver info in the modal", async () => {
+    const orderHeader = await $(".order-header-content");
+    await orderHeader.waitForDisplayed();
+    await expect(orderHeader).toBeDisplayed();
+  });
+});
--- wdio.conf.js
+++ wdio.conf.js
@@ -1,42 +1,44 @@
 exports.config = {
-    runner: 'local',
-    specs: [
-        './test/specs/**/*.js'
-    ],
-    exclude: [
-        // 'path/to/excluded/files'
-    ],
-    maxInstances: 10,
-    headless: true,
-    capabilities: [
+  runner: "local",
+  specs: ["./test/specs/**/*.js"],
+  exclude: [
+    // 'path/to/excluded/files'
+  ],
+  maxInstances: 10,
+  headless: true,
+  capabilities: [
     {
-        maxInstances: 5,
-        browserName: 'chrome',
-        acceptInsecureCerts: true,
-        'moz:firefoxOptions': {
-            //args: ['-headless']
-        }
-    }
-],
-    logLevel: 'error',
-    bail: 0,
-    baseUrl: 'https://cnt-51bf36a0-fb33-4b52-b9ff-a78abe6b7297.containerhub.tripleten-services.com',
-    waitforTimeout: 10000,
-    connectionRetryTimeout: 120000,
-    connectionRetryCount: 3,
-    services: [
-        ['chromedriver', {
-            logFileName: 'wdio-chromedriver.log', // default
-            outputDir: 'driver-logs', // overwrites the config.outputDir
-            args: ['--silent']
-        }],
-        'geckodriver', 
-        'intercept', 
-    ],
-    framework: 'mocha',
-    reporters: ['spec'],
-    mochaOpts: {
-        ui: 'bdd',
-        timeout: 60000
+      maxInstances: 5,
+      browserName: "chrome",
+      acceptInsecureCerts: true,
+      "goog:chromeOptions": {
+        args: ["headless", "disable-gpu"],
+      },
     },
-}
+  ],
+  logLevel: "error",
+  bail: 0,
+  baseUrl:
+    "https://cnt-51bf36a0-fb33-4b52-b9ff-a78abe6b7297.containerhub.tripleten-services.com",
+  waitforTimeout: 10000,
+  connectionRetryTimeout: 120000,
+  connectionRetryCount: 3,
+  services: [
+    [
+      "chromedriver",
+      {
+        logFileName: "wdio-chromedriver.log", // default
+        outputDir: "driver-logs", // overwrites the config.outputDir
+        args: ["--silent"],
+      },
+    ],
+    "geckodriver",
+    "intercept",
+  ],
+  framework: "mocha",
+  reporters: ["spec"],
+  mochaOpts: {
+    ui: "bdd",
+    timeout: 60000,
+  },
+};
